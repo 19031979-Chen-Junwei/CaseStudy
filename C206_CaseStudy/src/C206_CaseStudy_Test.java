16f70409fc8519bc563dded41b8f19ab81756684
elva1 <elva1@DESKTOP-95UUNI6>
2020-08-19 22:05:17 +0800

    hi new update

--- C206_CaseStudy/src/C206_CaseStudy_Test.java
+++ C206_CaseStudy/src/C206_CaseStudy_Test.java
@@ -1,4 +1,6 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
 
 import java.util.ArrayList;
 
@@ -27,9 +29,29 @@ public class C206_CaseStudy_Test {
 	}
 
 	@Test
-	public void addCamcorderTest() {
+	public void addProductTest() {
 		// Item list is not null, so that can add a new item - boundary
 		assertNotNull("Check if there is valid Camcorder arraylist to add to", productList);
+		// Given an empty list, after adding 1 item, the size of the list is 1 - normal
+		// The item just added is as same as the first item of the list
+		C206_CaseStudy.addProduct(productList, product1);
+		assertEquals("Check that product arraylist size is 1", 1, productList.size());
+		assertSame("Check that new product is added", product1, productList.get(0));
+	}
 
+	public void retrieveAllProductTest() {
+		// Test if product list is not null but empty -boundary
+		assertNotNull("Test if there is valid product arraylist to retrieve item", productList);
+
+		// test if the list of product retrieved from the SourceCentre is empty -
+		// boundary
+		String allproduct = C206_CaseStudy.viewAllProduct(productList);
+		String testOutput = "";
+		assertEquals("Check that ViewAllProductlist", testOutput, allproduct);
+	}
+	public void deleteProduct() {
+		
+		assertNotNull("Test if there is valid product arraylist to delete item", productList);
+		
 	}
 }
